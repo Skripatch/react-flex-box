a260642933c0903e7dc9e611ebd09d70fa7ff6eb
Yuri Friedberg <yuri@minutemedia.com>
2019-02-19 17:58:31 +0200

    minor fixes

--- src/Flex.js
+++ src/Flex.js
@@ -1,18 +1,22 @@
 import React from 'react';
 
+const TAG_NAME = 'div';
+const CONTAINER = 'row-flexStart-flexStart';
+const ITEM = '1-1-auto';
+const EMPTY = '';
 export const Flex = ({
   className,
   children,
-  tagName = 'div',
-  container = '',
-  item = '',
+  tagName = TAG_NAME,
+  container = CONTAINER,
+  item = ITEM,
   style = {},
   ...props,
 }) => {
   const TagName = tagName;
-  const [ flexDirection, justifyContent, alignItems, flexWrap ] = container.toLowerCase().split('-');
-  const [ flexGrow, flexShrink, flexBasis, justifySelf, alignSelf ] = item.toLowerCase().split('-');
-  const display = container !== '' ? 'flex' : 'initial';
+  const [ flexDirection, justifyContent, alignItems, flexWrap ] = container.split('-');
+  const [ flexGrow, flexShrink, flexBasis, justifySelf, alignSelf ] = item.split('-');
+  const display = container !== EMPTY ? 'flex' : 'initial';
   const combinedStyles = {
     display,
     flexDirection,
